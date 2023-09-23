2e4c8198fab9c257900d051f5e3fb052c6d77eb4
Antarpreet <pnarain82@gmail.com>
2023-09-23 10:42:46 +1000

    home

--- src/pages/user/Home.js
+++ src/pages/user/Home.js
@@ -1,9 +1,14 @@
 import React from "react";
-import Header from "../../components/layout/Header";
 import Footer from "../../components/layout/Footer";
+import { useSelector } from "react-redux";
 function Home() {
+  const { bouquetlist } = useSelector((state) => state.bouquet);
+  console.log(bouquetlist);
   return (
     <div>
+      {bouquetlist.map((item) => (
+        <img src={item.img} alt="bouquet" />
+      ))}
       <Footer />
     </div>
   );
