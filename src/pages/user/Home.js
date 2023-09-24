db2b476d0818ec87a5215ebd51be58664105c3e0
Antarpreet <pnarain82@gmail.com>
2023-09-24 17:49:51 +1000

    homepage

--- src/pages/user/Home.js
+++ src/pages/user/Home.js
@@ -1,14 +1,28 @@
 import React from "react";
 import Footer from "../../components/layout/Footer";
 import { useSelector } from "react-redux";
+import Carousel from "react-material-ui-carousel";
+import { Box } from "@mui/material";
+import Card from "../../components/card/Card";
 function Home() {
   const { bouquetlist } = useSelector((state) => state.bouquet);
   console.log(bouquetlist);
   return (
     <div>
-      {bouquetlist.map((item) => (
-        <img src={item.img} alt="bouquet" />
-      ))}
+      <Carousel>
+        {bouquetlist.map((item) => (
+          <img src={item.img} alt="bouquet" width={"100%"} height={"300px"} />
+        ))}
+      </Carousel>
+      <Box>Home Login/Signout Products</Box>
+      <Box>
+        <Box>side option</Box>
+        <Box>
+          {bouquetlist.map((item, i) => (
+            <Card key={i} {...item} />
+          ))}
+        </Box>
+      </Box>
       <Footer />
     </div>
   );
