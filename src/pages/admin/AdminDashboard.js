46b90be20f74dc8c714f33ac197b92cbba4e8671
Antarpreet <pnarain82@gmail.com>
2023-10-02 11:16:09 +1100

    jdhf

--- src/pages/admin/AdminDashboard.js
+++ src/pages/admin/AdminDashboard.js
@@ -1,10 +1,22 @@
 import React from "react";
 import AdminLayout from "../../components/layout/AdminLayout";
+import { getAllUserAction } from "../../redux_firebase/user/userAction";
+import { useDispatch, useSelector } from "react-redux";
+import { Typography } from "@mui/material";
 function AdminDashboard() {
+  const dispatch = useDispatch();
+  dispatch(getAllUserAction());
+  const { bouquetlist } = useSelector((state) => state.bouquet);
+  const { admin } = useSelector((state) => state.admin);
+  console.log(bouquetlist.length, " ", admin);
   return (
     <AdminLayout>
-      Hello admin dashboard lore enfjewufhuehbfjer juvhefkjnjkref
-      juhrekhnfkenfkejwkf hjebhfewnfkewnl
+      <Typography variant="h3">
+        Hello {admin.fname} {admin.lname}, Welocme to Dashboard
+      </Typography>
+      <Typography variant="body1">
+        Today, we have total {bouquetlist.length} categories of bouquets.
+      </Typography>
     </AdminLayout>
   );
 }
--- src/pages/admin/customers/Customers.js
+++ src/pages/admin/customers/Customers.js
@@ -1,14 +1,54 @@
 import React from "react";
 import AdminLayout from "../../../components/layout/AdminLayout";
-import { getAllUserAction } from "../../../redux_firebase/user/userAction";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
+import {
+  Box,
+  Table,
+  TableContainer,
+  Paper,
+  TableBody,
+  TableHead,
+  TableRow,
+  TableCell,
+} from "@mui/material";
 function Customers() {
   console.log("customers");
-  const dispatch = useDispatch();
-
-  dispatch(getAllUserAction());
-
-  return <AdminLayout>Customers Page</AdminLayout>;
+  const { usersList } = useSelector((state) => state.user);
+  console.log(usersList);
+  return (
+    <AdminLayout>
+      Customers Page
+      <Box component="div">
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>First Name</TableCell>
+                <TableCell align="right">Last Name</TableCell>
+                <TableCell align="right">Email</TableCell>
+                <TableCell align="right">Ph number</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {usersList.map((item) => (
+                <TableRow
+                  key={item.fname}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    {item.lname}
+                  </TableCell>
+                  <TableCell align="right">{item.fname} </TableCell>
+                  <TableCell align="right">{item.email}</TableCell>
+                  <TableCell align="right">{item.pno}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Box>
+    </AdminLayout>
+  );
 }
 
 export default Customers;
