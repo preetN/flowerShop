667bd93e5c43f4f789fa827b24e7753095cfaa03
Antarpreet <pnarain82@gmail.com>
2023-12-13 13:01:17 +1100

    card

--- src/components/card/CustomCard.js
+++ src/components/card/CustomCard.js
@@ -5,7 +5,6 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
-import ReactCardFlip from "react-card-flip";
 import { Box } from "@mui/material";
 import { Link } from "react-router-dom";
 
@@ -17,50 +16,38 @@ export default function CustomCard({
   date,
   price,
 }) {
-  const [flip, setFlip] = React.useState(false);
-
   return (
-    <Card
-      className="cardflip"
-      sx={{ width: 345 }}
-      // onHover={() => setFlip(!flip)}
-      onClick={() => setFlip(!flip)}
-    >
-      <ReactCardFlip isFlipped={flip} flipDirection="vertical">
-        <Box>
-          <CardMedia sx={{ height: 190 }} image={img} title="bouquet" />
-        </Box>
-        <Box sx={{ height: 190 }}>
-          <CardContent>
-            <Typography gutterBottom variant="h6" component="div">
-              {bname}
-            </Typography>
-            <Typography
-              variant="subtitle1"
-              color="text.secondary"
-              fontSize={"10px"}
-            >
-              {description}
-            </Typography>
-            <Typography variant="body1" color="secondary">
-              Price: ${price}
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              Date Added: {date}
-            </Typography>
-          </CardContent>
-          <CardActions sx={{ float: "right" }}>
-            <Link
-              to={`/bouquetdetails/${id}`}
-              style={{ textDecoration: "none" }}
-            >
-              <Button variant="contained" color="secondary" size="small">
-                More ...
-              </Button>
-            </Link>
-          </CardActions>
-        </Box>
-      </ReactCardFlip>
+    <Card className="card">
+      <Box className="card-img">
+        <CardMedia sx={{ height: 190 }} image={img} title="bouquet" />
+      </Box>
+      <Box className="card-body">
+        <CardContent>
+          <Typography gutterBottom variant="h6" component="div">
+            {bname}
+          </Typography>
+          <Typography
+            variant="subtitle1"
+            color="text.secondary"
+            fontSize={"10px"}
+          >
+            {description}
+          </Typography>
+          <Typography variant="body1" color="secondary">
+            Price: ${price}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Date Added: {date}
+          </Typography>
+        </CardContent>
+        <CardActions sx={{ float: "right" }}>
+          <Link to={`/bouquetdetails/${id}`} style={{ textDecoration: "none" }}>
+            <Button variant="contained" color="secondary" size="small">
+              More ...
+            </Button>
+          </Link>
+        </CardActions>
+      </Box>
     </Card>
   );
 }
